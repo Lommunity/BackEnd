e13e70aedd256a9a3396bb43d007b56852618bf3
leehyeeun <leehe0430@gmail.com>
2022-08-29 11:30:43 +0900

    git init

---
+++ src/main/java/com/Lommunity/LommunityApplication.java
@@ -0,0 +1,13 @@
+package com.Lommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lommunity/LommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
