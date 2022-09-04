ecd93cf4ff80f9096f5a952ddd7a86c927038e73
leehyeeun <leehe0430@gmail.com>
2022-09-04 21:08:36 +0900

    Region - RegionResponse 생성 - RegionController - searchRegion 구현

--- src/main/java/com/Lommunity/controller/region/RegionController.java
+++ src/main/java/com/Lommunity/controller/region/RegionController.java
@@ -1,16 +1,28 @@
 package com.Lommunity.controller.region;
 
-import com.Lommunity.domain.region.RegionRepository;
+import com.Lommunity.application.region.RegionService;
+import com.Lommunity.application.region.dto.RegionDto;
+import com.Lommunity.application.region.dto.RegionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/regions")
 @RequiredArgsConstructor
 public class RegionController {
 
-    private final RegionRepository regionRepository;
+    private final RegionService regionService;
 
+    @GetMapping("/search")
+    public RegionResponse searchRegion(@RequestParam("query") String query) {
+        // query → 검색어
+        return RegionResponse.builder()
+                             .searchRegions(regionService.findRegion(query))
+                             .build();
+    }
 }
