aced149291d1257eea2c3f1c15924d00f7abbffc
leehyeeun <leehe0430@gmail.com>
2022-09-22 17:29:24 +0900

    ec2-test-he - java -jar로 실행시 file을 읽어올 수 없어 InputStream을 사용하여 regions list를 가져오도록 수정 - test를 위한 /hello api에 접속할 수 있도록 SecurityConfig 수정

--- src/main/java/com/Lommunity/infrastructure/initresult/RegionInitializer.java
+++ src/main/java/com/Lommunity/infrastructure/initresult/RegionInitializer.java
@@ -8,8 +8,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -23,21 +24,24 @@ public class RegionInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         ClassPathResource classPathResource = new ClassPathResource("data/result.csv");
-        List<String> lines = Files.readAllLines(Paths.get(classPathResource.getURI()));
-        List<Region> regions = lines.stream()
-                                    .skip(1)
-                                    .map(line -> {
-                                        String[] split = line.split(",");
-                                        Long parentCode = Objects.equals(split[2], "") ? null : Long.parseLong(split[2]);
+        try (InputStream inputStream = classPathResource.getInputStream()) {
+            List<Region> regions = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines()
+                    .skip(1)
+                    .map(line -> {
+                        String[] split = line.split(",");
+                        Long parentCode = Objects.equals(split[2], "") ? null : Long.parseLong(split[2]);
 
-                                        return Region.builder()
-                                                     .code(Long.parseLong(split[1]))
-                                                     .level(Long.parseLong(split[0]))
-                                                     .parentCode(parentCode)
-                                                     .fullname(split[3])
-                                                     .build();
-                                    })
-                                    .collect(Collectors.toList());
-        regionRepository.saveAll(regions);
+                        return Region.builder()
+                                     .code(Long.parseLong(split[1]))
+                                     .level(Long.parseLong(split[0]))
+                                     .parentCode(parentCode)
+                                     .fullname(split[3])
+                                     .build();
+                    })
+                    .collect(Collectors.toList());
+            regionRepository.saveAll(regions);
+        }
+
     }
 }
