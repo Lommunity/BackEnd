19ffd2e568cc736cdb73913a2ecf7cb465c96d6b
leehyeeun <leehe0430@gmail.com>
2022-10-01 16:56:22 +0900

    User Info - test를 위한 FakeFileService와 프로덕션에서 사용되는 AwsS3FileService 생성, 빈 등록 - edit test 추가

--- src/test/java/com/Lommunity/application/user/UserServiceTest.java
+++ src/test/java/com/Lommunity/application/user/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.Lommunity.application.user;
 
+import com.Lommunity.application.file.dto.FileUploadRequest;
 import com.Lommunity.application.user.dto.RegisterRequest;
 import com.Lommunity.application.user.dto.RegisterResponse;
+import com.Lommunity.application.user.dto.UserEditRequest;
 import com.Lommunity.domain.region.RegionRepository;
 import com.Lommunity.domain.user.User;
 import com.Lommunity.domain.user.UserRepository;
@@ -27,7 +29,7 @@ class UserServiceTest {
     EntityTestHelper entityTestHelper;
 
     @Test
-    public void registerTest() {
+    public void register() {
         User user = userRepository.save(builder()
                 .nickname("이혜은")
                 .profileImageUrl("aaa")
@@ -40,9 +42,11 @@ class UserServiceTest {
         RegisterResponse response = userService.register(RegisterRequest.builder()
                                                                         .userId(user.getId())
                                                                         .nickname("순대곱창전골")
-                                                                        .profileImageUrl(null)
                                                                         .regionCode(2611051000L)
-                                                                        .build());
+                                                                        .build(),
+                FileUploadRequest.builder()
+                                 .filename("testImageUrl")
+                                 .build());
 
 
         User completeJoin = userRepository.findWithRegionById(user.getId()).get();
@@ -50,7 +54,45 @@ class UserServiceTest {
         // Join 후
         assertThat(completeJoin.getId()).isEqualTo(user.getId());
         assertThat(completeJoin.isRegistered()).isEqualTo(true);
-        assertThat(completeJoin.getProfileImageUrl()).isEqualTo(null);
-        assertThat(completeJoin.getRegion().getFullname()).isEqualTo("부산 중구 중앙동");
+        assertThat(completeJoin.getProfileImageUrl()).isEqualTo("testImageUrl");
+        assertThat(completeJoin.getRegion().getCode()).isEqualTo(2611051000L);
+    }
+
+    @Test
+    public void edit() {
+        // given
+        User user = userRepository.save(builder()
+                .nickname("이혜은")
+                .profileImageUrl("aaa")
+                .provider("naver")
+                .providerId("0430")
+                .role(UserRole.USER)
+                .registered(false)
+                .build());
+
+        RegisterResponse response = userService.register(RegisterRequest.builder()
+                                                                        .userId(user.getId())
+                                                                        .nickname("순대곱창전골")
+                                                                        .regionCode(2611051000L)
+                                                                        .build(),
+                FileUploadRequest.builder()
+                                 .filename("testImageUrl")
+                                 .build());
+
+        // when
+        UserEditRequest editRequest = UserEditRequest.builder()
+                                                     .nickname("apple")
+                                                     .profileImageUrl(user.getProfileImageUrl())
+                                                     .regionCode(2611060000L).build();
+        FileUploadRequest uploadRequest = FileUploadRequest.builder().filename("editImageFile").build();
+        userService.edit(user.getId(), editRequest, uploadRequest);
+
+        // then
+        User completeJoin = userRepository.findWithRegionById(user.getId()).get();
+        assertThat(completeJoin.getNickname()).isEqualTo("apple");
+        assertThat(completeJoin.getId()).isEqualTo(user.getId());
+        assertThat(completeJoin.isRegistered()).isEqualTo(true);
+        assertThat(completeJoin.getProfileImageUrl()).isEqualTo("editImageFile");
+        assertThat(completeJoin.getRegion().getCode()).isEqualTo(2611060000L);
     }
 }
