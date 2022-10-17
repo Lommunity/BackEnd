4b962b6c21917d5ba44d9d7cc4df773f692430eb
leehyeeun <leehe0430@gmail.com>
2022-10-17 12:16:17 +0900

    LikeTarget enum 수정

--- src/main/java/com/Lommunity/domain/like/LikeTarget.java
+++ src/main/java/com/Lommunity/domain/like/LikeTarget.java
@@ -1,12 +1,5 @@
 package com.Lommunity.domain.like;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum LikeTarget {
-    POST;
-
-    @JsonCreator
-    public static LikeTarget from(String s) {
-        return LikeTarget.valueOf(s.toUpperCase());
-    }
+    POST
 }
